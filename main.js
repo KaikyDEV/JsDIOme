0a55623e2f63d52e6f44b93d01409b9c69f34fd8
Kaiky Santos <kaikysantosdasilva38@gmail.com>
2024-02-03 17:48:03 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// como declarar  váriaveis dentro do JavaScript
+// variavel let é um tipo em que possa mudar logo depois
+// já o const, não posso fazer isso. É uma váriavel constante
+
+let variavel = 10 / 10;
+
+console.log(variavel);
+
