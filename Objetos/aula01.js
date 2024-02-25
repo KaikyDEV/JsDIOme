3b7d97ca8436f1a43454775c639856b6d2c43eac
Kaiky Santos <kaikysantosdasilva38@gmail.com>
2024-02-25 18:23:16 -0300

    exercicio em javascript

--- Objetos/aula01.js
+++ Objetos/aula01.js
@@ -15,7 +15,19 @@ class Pessoa{
     }
 }
 
+function compararPessoas(p1, p2){
+    if(p1.idade > p2.idade){
+        console.log(`${p1.nome} é maior que ${p2.nome}`)
+    }
+    else if(p2.idade > p1.idade){
+        console.log(`${p2.nome} é maior que ${p1.nome}`)
+    }
+    else{
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`)
+    }
+}
+
 const kaiky = new Pessoa('Kaiky', 19);
+const joao = new Pessoa('João', 20);
 
-
-kaiky.descrever();
+compararPessoas(kaiky, joao);
---
+++ Objetos/exercicio01.js
@@ -0,0 +1,23 @@
+/*
+
+1 - Crie uma classe para representar carros.
+Os carros possuem uma marca, uma cor e um gasto médiode combustível por Kilômetros rodado.
+Crie um método que dado a quantidade de quilômetros e o preço o combustível nos dê o valor gasto
+em reais para realizar este percurdo.
+
+*/
+
+class Carro{
+    marca;
+    cor;
+    gastoMedioPorKM;
+
+    constructor(marca, cor, gastoMedioPorKM){
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKM;
+    }
+
+}
+
+const uno = new Carro('Fiat', 'Prata', 1/12);
