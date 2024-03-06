c90681721b775274f30ba7448a9f5064bfab8032
Kaiky Santos <kaikysantosdasilva38@gmail.com>
2024-03-06 14:23:31 -0300

    Notas

--- Listas (arrays)/aula02.js
+++ Listas (arrays)/aula02.js
@@ -1,9 +1,21 @@
-const nome = `Kaiky Santos da Silva`;
+const notas = [];
 
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
 
-for (let i = 0; i < nome.length; i++) {
-    const element = nome[i];
-    console.log(element);
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+
+    soma = soma + nota;
 
 }
 
+const media = soma / notas.length
+
+console.log(media);
