585049b7aead1b9f390c808129f254be549089f5
Kaiky Santos <kaikysantosdasilva38@gmail.com>
2024-03-09 20:56:27 -0300

    Sorteio de numeros

--- Importacao/main.js
+++ Importacao/main.js
@@ -1,3 +1,23 @@
-const funcoes = require('./funcoes_auxiliares');
+const { gets, print} = require('./funcoes_auxiliares');
 
-console.log(funcoes.gets());
+
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+
+    const numeroSorteado = gets();
+
+    numerosSorteados.push(numeroSorteado);
+}
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteado = numerosSorteados[i];
+
+    if (numeroSorteado > maiorValor) {
+        maiorValor = numeroSorteado
+    }
+}
+
+print(maiorValor);
